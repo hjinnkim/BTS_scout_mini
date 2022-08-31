d7a2927e1967a985b57a768e17ec91ed4d171c8e
hjinnkim <khj12203@gmail.com>
2022-08-31 18:55:25 +0900

    Rename install_dependencies, update .gitigonore

--- install_dependencies_noetic.sh
+++ install_dependencies_noetic.sh
@@ -3,10 +3,11 @@
 # Install package scout-gazebo simulation dependencies
 sudo apt-get install libasio-dev ros-noetic-ros-control ros-noetic-ros-controllers ros-noetic-gazebo-ros ros-noetic-gazebo-ros-control ros-noetic-joint-state-publisher-gui ros-noetic-teleop-twist-keyboard 
 
-# Next package build 
+# TODO: Next package build 
 # ros-noetic-navigation
 
 # slam-gmapping package download
+# Reference site : http://wiki.ros.org/gmapping
 # Reference site : https://aur.archlinux.org/packages/ros-noetic-slam-gmapping
 # Reference site : https://aur.archlinux.org/packages/ros-noetic-openslam-gmapping
 mkdir slam-gmapping
@@ -20,3 +21,8 @@ tar -xvf ros-noetic-openslam-gmapping-0.2.1.tar.gz
 
 cd ..
 
+# navigation package download
+# Reference site : http://wiki.ros.org/navigation
+# Reference site : https://github.com/ros-planning/navigation
+
+git clone https://github.com/ros-planning/navigation.git
